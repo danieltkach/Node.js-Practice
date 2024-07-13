843ffb6a935b042999ee62d27294700c248ebd51
Daniel Tkach <danieltkach@gmail.com>
2024-07-13 20:54:58 -0300

    threading

--- threads.js
+++ threads.js
@@ -1,90 +1,49 @@
 const crypto = require('crypto');
-process.env.UV_THREADPOOL_SIZE = 4;
+
+process.env.UV_THREADPOOL_SIZE = 2;
+const threadpoolSize = process.env.UV_THREADPOOL_SIZE;
 
 let durations = [];
 const totalOperations = 8;
 let completedOperations = 0;
 
 function printDuration() {
-    if (completedOperations === totalOperations) {
-        const total = durations.reduce((a, c) => a += c, 0);
-        console.log('Duration: ', total);
+    let total = 0;
+    let durationsByThread = [];
+    for (let i = 0; i < totalOperations / threadpoolSize; i++) {
+        let max = durations[i];
+        for (let j = threadpoolSize * i; j < threadpoolSize; j++) {
+            if (durations[i] > max) {
+                max = durations[i];
+            }
+        }
+        durationsByThread.push(max);
     }
+    total = durationsByThread.reduce((a, c) => a += c);
+    console.log('Duration: ', total);
+    completedOperations = 0;
+    durations.length = 0;
 }
 
 function runPbkdf2(times) {
-    const start = Date.now();
     for (let i = 0; i < times; i++) {
+        const start = Date.now();
         crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
             const duration = Date.now() - start;
             durations.push(duration);
             console.log(`${i + 1}: `, duration);
             completedOperations++;
-            printDuration();
+            if (completedOperations === totalOperations) {
+                printDuration();
+            }
         });
     }
 }
 
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    const duration = Date.now() - start;
-    durations.push(duration);
-    console.log('1: ', duration);
-    completedOperations++;
-    printDuration();
-});
+function testThreads() {
+    console.log(`Testing with threadpool size: ${threadpoolSize}`);
+    runPbkdf2(8);
+}
 
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    const duration = Date.now() - start;
-    durations.push(duration);
-    console.log('2: ', duration);
-    completedOperations++;
-    printDuration();
-});
 
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    const duration = Date.now() - start;
-    durations.push(duration);
-    console.log('3: ', duration);
-    completedOperations++;
-    printDuration();
-});
-
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    const duration = Date.now() - start;
-    durations.push(duration);
-    console.log('4: ', duration);
-    completedOperations++;
-    printDuration();
-});
-
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    const duration = Date.now() - start;
-    durations.push(duration);
-    console.log('5: ', duration);
-    completedOperations++;
-    printDuration();
-});
-
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    const duration = Date.now() - start;
-    durations.push(duration);
-    console.log('6: ', duration);
-    completedOperations++;
-    printDuration();
-});
-
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    const duration = Date.now() - start;
-    durations.push(duration);
-    console.log('7: ', duration);
-    completedOperations++;
-    printDuration();
-});
-
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    const duration = Date.now() - start;
-    durations.push(duration);
-    console.log('8: ', duration);
-    completedOperations++;
-    printDuration();
-});
+testThreads();
